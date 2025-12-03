cb3706b756c2ea086f874a04218563b3012cb563
Ehsan Irani <ehsan.irani@protonmail.com>
2025-12-03 13:30:56 +0100

    feat: add VS Code-style terminal toggle at bottom
    - Configure vterm-toggle to display at bottom of window (30% height)
    - Change keybinding to C-` (similar to VS Code's Ctrl+`)
    - Set terminal scope to project for better workflow
    - Add display-buffer-alist rule for consistent bottom placement

--- modules/ee-terminals.el
+++ modules/ee-terminals.el
@@ -9,14 +9,28 @@
   :config
   (setq vterm-shell (getenv "SHELL")))
 
-;; Vterm toggle - use default behavior
+;; Vterm toggle - VS Code style bottom terminal
 (use-package vterm-toggle
   :straight t
   :after vterm
+  :custom
+  (vterm-toggle-fullscreen-p nil)
+  (vterm-toggle-scope 'project)
   :bind
-  (("C-c t" . vterm-toggle)
+  (("C-`" . vterm-toggle)
    ("C-c T" . vterm-toggle-cd)
    :map vterm-mode-map
-   ("C-c t" . vterm-toggle)))
+   ("C-`" . vterm-toggle))
+  :config
+  ;; Show vterm at bottom with 30% height
+  (add-to-list 'display-buffer-alist
+               '((lambda (buffer-or-name _)
+                   (let ((buffer (get-buffer buffer-or-name)))
+                     (with-current-buffer buffer
+                       (or (equal major-mode 'vterm-mode)
+                           (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
+                 (display-buffer-reuse-window display-buffer-at-bottom)
+                 (reusable-frames . visible)
+                 (window-height . 0.3))))
 
 (provide 'ee-terminals)
